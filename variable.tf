d1f95c4f64fd99e6b56379dd3309bc7bfaef50a6
Andrew Bounds <392383+p0rkjello@users.noreply.github.com>
2022-06-06 16:34:03 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "image" {
+type = string
+default = "ami-0603cbe34fd08cb81"
+}
+
+variable "hardware" {
+default = "t2.micro"
+}
+
+variable "name" {
+default = "bounds-20220606"
+}
+
+variable "bucketname" {
+default = "bucket-name-bounds-bucket-222"
+}
